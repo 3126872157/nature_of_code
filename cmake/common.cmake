18c8cbf37ae0afd61b5a6ebe2586f46d803d7ab6
3126872157 <3126872157@qq.com>
2026-01-24 10:20:50 +0800

    [feat]支持win和linux

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -4,15 +4,24 @@
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-#可执行文件输出目录
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+if (WIN32)
+    set(OS "win32")
+    set(SFML_DIR "D:/DevEnv/Libraries/SFML-3.0.2")
+    find_package(SFML 3 REQUIRED COMPONENTS Graphics Window System)
+elseif (UNIX AND NOT APPLE)
+    set(OS "linux")
+    #可执行文件输出目录
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
-#SFML 库拉取配置
-include(FetchContent)
-FetchContent_Declare(SFML
-        GIT_REPOSITORY https://github.com/SFML/SFML.git
-        GIT_TAG 3.0.1
-        GIT_SHALLOW ON
-        EXCLUDE_FROM_ALL
-        SYSTEM)
-FetchContent_MakeAvailable(SFML)
+    #SFML 库拉取配置
+    include(FetchContent)
+    FetchContent_Declare(SFML
+            GIT_REPOSITORY https://github.com/SFML/SFML.git
+            GIT_TAG 3.0.1
+            GIT_SHALLOW ON
+            EXCLUDE_FROM_ALL
+            SYSTEM)
+    FetchContent_MakeAvailable(SFML)
+endif ()
+
+message("OS is ${OS}")
